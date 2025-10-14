ab669e5a2cb931e3d612fa7b6a718c0c1800b4c3
caiquegressoni <caiquegressoni@gmail.com>
2025-10-14 12:54:46 -0300

    Finalizado a primeira aula do curso

---
+++ PrimeiroCodigo/PrimeiroCodigo/src/Operacoes.java
@@ -0,0 +1,12 @@
+public class Operacoes {
+    public static void main(String[] args) {
+        int number1 = 10;
+        int number2 = 5;
+
+        int sum = number1 + number2;
+        int subtraction = number1 - number2;
+
+        System.out.printf("A soma é %d%n", sum);
+        System.out.printf("A subtração é %d%n", subtraction);
+    }
+}
---
+++ PrimeiroCodigo/PrimeiroCodigo/src/Perfil.java
@@ -0,0 +1,7 @@
+public class Perfil {
+    public static void main(String[] args) {
+        String name = "Caique";
+
+        System.out.printf("Meu nome é %s%n", name);
+    }
+}
