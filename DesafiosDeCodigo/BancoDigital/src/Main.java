9d6b3dc662b63f03430f760253598c1d4987330f
caiquegressoni <caiquegressoni@gmail.com>
2025-11-05 08:35:10 -0300

    Iniciado projeto Banco Digital

---
+++ DesafiosDeCodigo/BancoDigital/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+     //TODO: cirar as seguintes funções; recebeTransferencia(), fazTransferencia(), enviaPix(), recebePix()
+        String nomeCliente;
+        String [] tipoConta = {"Corrente", "Poupança"};
+        double saldoInicial = 2.500;
+
+        System.out.println("****************Cadastro****************");
+        System.out.println("Digite o nome do cliente");
+        nomeCliente = sc.next();
+        System.out.println("Informe o tipod da conta");
+    }
+}
